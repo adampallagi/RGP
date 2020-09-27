1e06e7693181527e7b254c7ca7c5205651c68660
György Dóra <gyorgydora99@gmail.com>
2020-09-27 15:04:04 +0300

    Added more 6 JUnit tests to BasicMathTest

--- CrossPlatformCalculator/src/test/java/providers/BasicMathTest.java
+++ CrossPlatformCalculator/src/test/java/providers/BasicMathTest.java
@@ -33,4 +33,46 @@ class BasicMathTest {
         assertNotEquals(3, new BasicMath().divide(25, 5));
         assertEquals(4, new BasicMath().divide(28, 7));
     }
+
+    @Test
+    void testSin() {
+        assertNotEquals(5, new BasicMath().sin(9));
+        assertNotEquals(2, new BasicMath().sin(10));
+        assertEquals(0.01745240643728351, new BasicMath().sin(1));
+    }
+
+    @Test
+    void testCos() {
+        assertNotEquals(6, new BasicMath().cos(2));
+        assertNotEquals(23, new BasicMath().cos(4));
+        assertEquals(0.9998476951563913, new BasicMath().cos(1));
+    }
+
+    @Test
+    void testTan() {
+        assertNotEquals(7, new BasicMath().tan(2.4));
+        assertNotEquals(8, new BasicMath().tan(6.5));
+        assertEquals(0.02618592156918693, new BasicMath().tan(1.5));
+    }
+
+    @Test
+    void testCtg() {
+        assertNotEquals(9, new BasicMath().ctg(4.7));
+        assertNotEquals(6, new BasicMath().ctg(3.3));
+        assertEquals(38.1884592970256, new BasicMath().ctg(1.5));
+    }
+
+    @Test
+    void testMod() {
+        assertNotEquals(2, new BasicMath().mod(8, 2));
+        assertNotEquals(7, new BasicMath().mod(9, 6));
+        assertEquals(4, new BasicMath().mod(24, 5));
+    }
+
+    @Test
+    void testPower() {
+        assertNotEquals(5, new BasicMath().power(3, 2));
+        assertNotEquals(34, new BasicMath().power(6, 7));
+        assertEquals(625, new BasicMath().power(5, 4));
+    }
 }
